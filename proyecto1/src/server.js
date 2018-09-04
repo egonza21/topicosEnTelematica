9402a89790268f417667a8a2dc138ea4d59a2fb1
Efrain Gonzalez Arias <egonza21@eafit.edu.co>
2018-09-04 09:43:49 -0700

    ts

--- proyecto1/src/server.js
+++ proyecto1/src/server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: false}));
 // required for passport
 app.use(session({
-	secret: 'faztwebtutorialexample',
+	secret: 'egonza21',
 	resave: false,
 	saveUninitialized: false
 }));
